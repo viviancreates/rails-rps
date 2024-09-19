b46a8b7a616bbc4759733dcef9158f4e1ab7ae3b
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-19 20:52:10 +0000

    Add files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+  get ("/rock", {:controller => "", :action => ""} )
+    
+
+  get ("/rock", {:controller => "", :action => ""} )
+
+
+  get ("/rock", {:controller => "", :action => ""} )
+
 end
