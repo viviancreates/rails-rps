8d959c9da3e9d4305f3c79273a5ba9a2cb149bf5
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-20 21:16:22 +0000

    One = is to assign, two == is to compare

--- app/controllers/rock_controller.rb
+++ app/controllers/rock_controller.rb
@@ -5,9 +5,9 @@ class RockController < ApplicationController
 
     @comp_move = moves.sample
 
-    if @comp_move = "rock"
+    if @comp_move == "rock"
       @outcome = "tied"
-    elsif @comp_move = "paper"
+    elsif @comp_move == "paper"
       @outcome = "lost"
     else
       @outcome = "won"
